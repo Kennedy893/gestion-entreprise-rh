9fb8c4cef75bd724efa48e589af7a6a4458c0087
Rakotoarilolona <rakotoarilolonahasiniaina@gmail.com>
2025-11-29 08:17:02 +0300

    debug notes

--- app/models/HdashModel.php
+++ app/models/HdashModel.php
@@ -178,27 +178,105 @@ class HdashModel {
             $presences = Flight::HModel()->get_generalised(
                 "presence",
                 "*",
-                ["id_employe"],
-                [$id_employe],
+                ["id_employe","date_travail"],
+                [$id_employe,$day['date']],
                 "",
                 []
-            );
+            );    
             
             if($presences==null)
             {
                 $notes[]=[
-                    'ponctualite'=>1,
-                    'gestion_temps'=>1,
-                    'productivite'=>1
+                    'ponctualite'=>0,
+                    'gestion_temps'=>0,
+                    'productivite'=>0
                 ];
             }
             else
             {
-                $notes[]=[
-                    'ponctualite'=>3,
-                    'gestion_temps'=>3,
-                    'productivite'=>3
-                ];
+
+                foreach($presences as $presence)
+                {
+                    if($presence['id']!=null)
+                    {
+                        $heure_arrivee=strtotime($presence['entree']);                
+                        $config_poste = Flight::HpresenceModel()->get_config_poste($id_employe,$day['date']);
+                        $heure_poste_arrivee=strtotime($config_poste['entree']);
+                        $ponctualite=5;
+                        $gestion_temps=5;
+                        $productivite=5;
+                        if($heure_arrivee > $heure_poste_arrivee)
+                        {
+                            $diff=($heure_arrivee - $heure_poste_arrivee)/60;
+                            if($diff <=15)
+                            {
+                                $ponctualite=4;
+                            }
+                            else if($diff >15 && $diff <=30)
+                            {
+                                $ponctualite=3;
+                            }
+                            else if($diff >30 && $diff <=60)
+                            {
+                                $ponctualite=2;
+                            }
+                            else
+                            {
+                                $ponctualite=1;
+                            }
+                        }
+                        $duree_travail=0;
+                        $note_temps=[];
+                        $note_temps['flux']=5;
+                        $note_temps['time']=5;
+                        if(count($presences)>2)
+                        {
+                            $note_temps['flux']=$note_temps['flux']-(count($presences)-2)/2;
+                        }
+                        for($i=0 ; $i<count($presences) ; $i++)
+                        {
+                            $duree_travail+=(strtotime($presences[$i]['sortie']) - strtotime($presences[$i]['entree']))/60;
+                            $norme_sortie=strtotime($config_poste['sortie'])+(3600*3);
+                            if(strtotime($presences[$i]['sortie'])>= $norme_sortie)
+                            {
+                                $difference_sortie=(strtotime($presences[$i]['sortie']) - $norme_sortie)/3600;
+                                $note_temps['time']=$note_temps['time']-$difference_sortie;
+                            }
+
+                        }
+                        $gestion_temps=($note_temps['flux'] + $note_temps['time'])/2;
+                        $duree_poste=(strtotime($config_poste['duree_travail']))*60;
+                        if($duree_travail >= $duree_poste)
+                        {
+                            $productivite=5;
+                        }
+                        else
+                        {
+                            $ratio=$duree_travail / $duree_poste;
+                            if($ratio >=0.9)
+                            {
+                                $productivite=4;
+                            }
+                            else if($ratio >=0.75)
+                            {
+                                $productivite=3;
+                            }
+                            else if($ratio >=0.5)
+                            {
+                                $productivite=2;
+                            }
+                            else
+                            {
+                                $productivite=1;
+                            }
+                        }
+                        $note[]=[
+                            'ponctualite'=>$ponctualite,
+                            'gestion_temps'=>$gestion_temps,
+                            'productivite'=>$productivite
+                        ];
+                    }
+                }
             }
             
         }
