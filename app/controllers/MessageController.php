0bb2f0882ff3f5d7e9a0d255ab3854b435f9471a
Toky-Lionnel <tokyrajaonarivony@gmail.com>
2025-12-08 16:56:20 +0300

    Update MessageController.php

--- app/controllers/MessageController.php
+++ app/controllers/MessageController.php
@@ -32,8 +32,7 @@ class MessageController {
         $id_employe = $_POST['id_employe'];
 
         Flight::MessageModel()->envoyerMessage($id_employe, $contenu, 0);
-
-        Flight::redirect(constant('BASE_URL').'employe/messagerie?id_employe=' . $id_employe);
+        Flight::redirect(constant('BASE_URL').'vers_messagerie?id_employe=' . $id_employe);
     }
 
 
